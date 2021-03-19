960c4c7f659ee028f1a2863eec99c781118812e0
James Morgan <MorganJay@users.noreply.github.com>
2021-03-19 18:14:11 +0100

    Add project files.

---
+++ vid19/Controllers/Covid19Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vid19.Models;
+using vid19.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace vid19.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Covid19Controller : ControllerBase
+    {
+        private readonly Covid19Services _covid19Service;
+
+        public Covid19Controller(Covid19Services covid19Services)
+        {
+            _covid19Service = covid19Services;
+        }
+
+        // GET: api/<Covid19Controller>
+        [HttpGet]
+        public async Task<ActionResult<Stats>> World()
+        {
+            return await _covid19Service.GetWorldStats();
+        }
+    }
+}
---
+++ vid19/Services/Covid19Services.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using vid19.Models;
+
+namespace vid19.Services
+{
+    public class Covid19Services
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public async Task<Stats> GetWorldStats()
+        {
+            var worldStatTask = client.GetStreamAsync("https://api.covid19api.com/world/total");
+
+            var worldStats = await JsonSerializer.DeserializeAsync<Stats>(await worldStatTask);
+            return worldStats;
+        }
+    }
+}
